70128de73d5217642da197d0e6d54c73df2512e7
Terry Bonds <terry_bonds@yahoo.co.uk>
2023-06-05 10:48:19 +0100

    Fix zero in unvisited sections

--- tests/tests.py
+++ tests/tests.py
@@ -6,38 +6,56 @@ import pathlib as pl
 
 
 # command line
-path_command_line = "python pathway_tester.py -i {0} -c tests/test-config.json"
+path_command_line = "python pathway_tester.py -i {0} -r 2 -c tests/test-config.json"
 exits_command_line = "python exit_checker.py -i {0} -s 1 -c tests/test-config.json"
 imager_command_line = "python branch_imager.py -i {0} -c tests/test-config.json"
-path_input_file = "tests/data/test.pdf"
-path_output_file = "pathing-tests/pass_1_test_report.html"
+test_input_file = "tests/data/test.pdf"
+path_output_file1 = "pathing-tests/pass_1_test_report.html"
+path_output_file2 = "pathing-tests/pass_2_test_report.html"
 exits_output_file = "exit-checks/exit_check_test_report.html"
 image_output_file = "branch-imaging/branching.png"
 
 
 class Paths(TestCase):
     """ Test cases for pathing tester """
-    # test creation of pathing test
+    # test pathing test file
     def test_pathway_tester_content(self):
-        os.system(f"{path_command_line.format(path_input_file)}")
-        with open(path_output_file, "r", encoding="utf-8") as path:
+        os.system(f"{path_command_line.format(test_input_file)}")
+        with open(path_output_file1, "r", encoding="utf-8") as path:
             contents = path.read()
             self.assertIn("<b>Last section visited:</b> 5 (last section in adventure is 5)" , contents)
 
 
+    # test pathing logic
+    def test_pathway_tester_logic(self):
+        os.system(f"{path_command_line.format(test_input_file)}")
+        with open(path_output_file2, "r", encoding="utf-8") as path:
+            contents = path.read()
+            self.assertIn("has been previously visited, moving on." , contents)
+
+
+    # test no zero in unvisited sections
+    def test_pathway_tester_no_zero(self):
+        os.system(f"{path_command_line.format(test_input_file)}")
+        with open(path_output_file2, "r", encoding="utf-8") as path:
+            contents = path.read()
+            self.assertNotIn("<b>Sections not visited by any journey:</b> [0" , contents)
+
+
 class Exits(TestCase):
     """ Test cases for exit tester """
-    # test creation of pathing test
+    # test creation of exit test file
     def test_exist_tester_length(self):
-        os.system(f"{exits_command_line.format(path_input_file)}")
+        os.system(f"{exits_command_line.format(test_input_file)}")
         with open(exits_output_file, "r", encoding="utf-8") as path:
             # check file length
             check_value = len(path.read())
             self.assertEqual(check_value, 1056)
 
 
+    # test correct orphaned section found
     def test_exits_tester_content(self):
-        os.system(f"{exits_command_line.format(path_input_file)}")
+        os.system(f"{exits_command_line.format(test_input_file)}")
         with open(exits_output_file, "r", encoding="utf-8") as path:
             # check correct section found
             contents = path.read()
@@ -46,8 +64,8 @@ class Exits(TestCase):
 
 class Images(TestCase):
     """ Test cases for branch imager """
-    # test creation of pathing test
+    # test creation of imaging file
     def test_imaging_file_exists(self):
-        os.system(f"{imager_command_line.format(path_input_file)}")
+        os.system(f"{imager_command_line.format(test_input_file)}")
         path = pl.Path(image_output_file)
         self.assertEqual((str(path), path.is_file()), (str(path), True))
